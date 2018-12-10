4ec01aca062adebcc05f3f20dfe8793ec6a04210
sergioeabarcaf <sergioaf1991@gmail.com>
2018-12-10 16:43:41 -0300

    WEB - creacion de paginas bases y conexion al app-routing.module

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewComponent } from './pages/new/new.component';
+import { LiveComponent } from './pages/live/live.component';
+import { InitComponent } from './pages/init/init.component';
+import { SessionComponent } from './pages/session/session.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'new', component: NewComponent },
+  { path: 'live', component: LiveComponent },
+  { path: 'init', component: InitComponent },
+  { path: 'session/:id', component: SessionComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InitComponent } from './pages/init/init.component';
+import { NewComponent } from './pages/new/new.component';
+import { LiveComponent } from './pages/live/live.component';
+import { SessionComponent } from './pages/session/session.component';
+import { NavbarComponent } from './pages/shared/navbar/navbar.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InitComponent,
+    NewComponent,
+    LiveComponent,
+    SessionComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
