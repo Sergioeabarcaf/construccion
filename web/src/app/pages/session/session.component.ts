a9b14fe762256385c5a3d88a99361a16c1738781
sergioeabarcaf <sergioaf1991@gmail.com>
2019-01-03 15:50:26 -0300

    WEB - Mostrar datos de medicion en una tabla en pagina session

--- web/src/app/pages/session/session.component.ts
+++ web/src/app/pages/session/session.component.ts
@@ -10,15 +10,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SessionComponent implements OnInit {
 
   session = null;
+  dataSession = [];
 
   constructor(public _firebase: FirebaseService, public activatedRoute: ActivatedRoute, public router: Router) {
     this.activatedRoute.params.subscribe( param => {
       this.session = this._firebase.getSession(param.id);
+      this.getData();
       console.log(this.session);
     });
   }
 
   ngOnInit() {
+  }
+
+  getData() {
+    Object.values(this.session.data).forEach( value => {
+      this.dataSession.push(value);
+    });
   }
 
 }
