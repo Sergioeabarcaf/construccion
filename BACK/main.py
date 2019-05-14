d87bc0e8413583a46289a6050732438cc8221bc2
sergioeabarcaf <sergioaf1991@gmail.com>
2019-05-14 16:27:48 -0400

    eliminar try de main

--- BACK/main.py
+++ BACK/main.py
@@ -21,42 +21,41 @@ value = {key : converter.getTimestamp()}
 firebase.clean(value, 'start')
 
 while(True):
-    try:
-        # Validar que exista conexion a internet
-        if( conection.valid() ):
-            # Consultar si se ha enviado una nueva sesion desde la web app
-            init = firebase.start()
-            if init != 0:
-                interval = int(init['intervalTime']) - 2
+    # Validar que exista conexion a internet
+    if( conection.valid() ):
+        # Consultar si se ha enviado una nueva sesion desde la web app
+        init = firebase.start()
+        if init != 0:
+            interval = int(init['intervalTime']) - 2
 
-                # Obtener timestamp actual
-                startTime = converter.getTimestamp()
+            # Obtener timestamp actual
+            startTime = converter.getTimestamp()
 
-                numberSession = str(firebase.numberSession())
-                dir = 'sessions/S-' + numberSession
-                firebase.setInfoSession(dir,init,startTime)
-                dirFile = csvFile.createFile(dir,init['material'],startTime, init)
+            numberSession = str(firebase.numberSession())
+            dir = 'sessions/S-' + numberSession
+            firebase.setInfoSession(dir,init,startTime)
+            dirFile = csvFile.createFile(dir,init['material'],startTime, init)
 
-                # Funcionamiento en modo manual
-                if init['finishedType'] == 'manual':
-                    # Esperar a que el usuario termine desde la WebApp
-                    while (firebase.endManualFromWebApp(numberSession)):
-                        getData(dir, dirFile)
-                        time.sleep(interval)
-                # Funcionamiento en modo automatico
-                elif init['finishedType'] == 'programado':
-                    # Obtener fecha y hora de finalizacion en tipo Date.
-                    finishedDate = converter.finishDate(init['finishedDate'] + ' ' + init['finishedTime'] )
-                    # Mientras hora actual sea menor o igual a finishedDate y el usuario no
-                    # haya terminado desde la aplicacion, realizar mediciones en los intervalos
-                    while( finishedDate >= converter.nowDateTime() and firebase.endManualFromWebApp(numberSession) ):
-                        getData(dir, dirFile)
-                        time.sleep(interval)
-                    # Si el sistema se detuvo por comparacion de fecha, cerrar el registro de ejecucion en firebase
-                    if (finishedDate < converter.nowDateTime() ):
-                        firebase.execManualEnd(numberSession)
-    # Si existe un error, enviar el timestamp del error a firebase
-    except:
-        key = str(n) + '-timestamp'
-        value = {key : converter.getTimestamp()}
-        firebase.clean(value, 'error')
+            # Funcionamiento en modo manual
+            if init['finishedType'] == 'manual':
+                # Esperar a que el usuario termine desde la WebApp
+                while (firebase.endManualFromWebApp(numberSession)):
+                    getData(dir, dirFile)
+                    time.sleep(interval)
+            # Funcionamiento en modo automatico
+            elif init['finishedType'] == 'programado':
+                # Obtener fecha y hora de finalizacion en tipo Date.
+                finishedDate = converter.finishDate(init['finishedDate'] + ' ' + init['finishedTime'] )
+                # Mientras hora actual sea menor o igual a finishedDate y el usuario no
+                # haya terminado desde la aplicacion, realizar mediciones en los intervalos
+                while( finishedDate >= converter.nowDateTime() and firebase.endManualFromWebApp(numberSession) ):
+                    getData(dir, dirFile)
+                    time.sleep(interval)
+                # Si el sistema se detuvo por comparacion de fecha, cerrar el registro de ejecucion en firebase
+                if (finishedDate < converter.nowDateTime() ):
+                    firebase.execManualEnd(numberSession)
+# # Si existe un error, enviar el timestamp del error a firebase
+#     except:
+#         key = str(n) + '-timestamp'
+#         value = {key : converter.getTimestamp()}
+#         firebase.clean(value, 'error')
