d6816d5bf0c575effd018214c98c859406cbae9f
sergioeabarcaf <sergioaf1991@gmail.com>
2019-05-20 10:48:15 -0400

    BACK - Uso de try y envio de excepciones a firebase

--- BACK/main.py
+++ BACK/main.py
@@ -1,3 +1,4 @@
+import sys
 import firebase
 import converter
 import sensor
@@ -14,11 +15,8 @@ def getData(dir, dirFile):
     firebase.pushData(dir, data)
     csvFile.writeData(dirFile, data)
 
-# Obtener el numero de intento para loguear y Detener la ejecucion de cualquier sesion al iniciar el programa
-n = firebase.getNumberLog()
-key = str(n) + '-timestamp'
-value = {key : converter.getTimestamp()}
-firebase.clean(value, 'start')
+# Detener la ejecucion de cualquier sesion al iniciar el programa
+firebase.clean(converter.getTimestamp(), 'start')
 
 while(True):
     try:
@@ -46,7 +44,7 @@ while(True):
                 # Funcionamiento en modo automatico
                 elif init['finishedType'] == 'programado':
                     # Obtener fecha y hora de finalizacion en tipo Date.
-                    finishedDate = converter.finishDate(init['finishedDate'] + ' ' + init['finishedTime'] )
+                    finishedDate = converter.finishDate(init['finishedDate'], init['finishedTime'] )
                     # Mientras hora actual sea menor o igual a finishedDate y el usuario no
                     # haya terminado desde la aplicacion, realizar mediciones en los intervalos
                     while( finishedDate >= converter.nowDateTime() and firebase.endManualFromWebApp(numberSession) ):
@@ -55,8 +53,8 @@ while(True):
                     # Si el sistema se detuvo por comparacion de fecha, cerrar el registro de ejecucion en firebase
                     if (finishedDate < converter.nowDateTime() ):
                         firebase.execManualEnd(numberSession)
-    # Si existe un error, enviar el timestamp del error a firebase
+# Si existe un error, enviar el timestamp del error a firebase y el tipo de error
     except:
-        key = str(n) + '-timestamp'
-        value = {key : converter.getTimestamp()}
-        firebase.clean(value, 'error')
+        print(sys.exc_info())
+        print("Error inesperado:", sys.exc_info()[0])
+        firebase.clean(converter.getTimestamp(), 'error', str(sys.exc_info()[0]))
