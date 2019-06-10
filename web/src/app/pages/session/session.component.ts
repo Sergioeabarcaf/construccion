e0c0704be6d721268fc7fc82f481bb10b12d9308
sergioeabarcaf <sergioaf1991@gmail.com>
2019-06-10 17:17:35 -0400

    WEB - mostrar sesion segun numero de sesion

--- web/src/app/pages/session/session.component.ts
+++ web/src/app/pages/session/session.component.ts
@@ -9,28 +9,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SessionComponent implements OnInit {
 
-  session = null;
-  dataSession = [];
-
   constructor(public _firebase: FirebaseService, public activatedRoute: ActivatedRoute, public router: Router) {
     this.activatedRoute.params.subscribe( param => {
-      // Obtener la sesion correspondiente al id solicitado.
-      this.session = this._firebase.getSession(param.id);
-      // Traspasar los datos a un array.
-      this.getData();
-      console.log(this.session);
+      // Actualizar los valores de current en firebase.service con los de la ultima sesion.
+      this._firebase.getInfoLargeCurrent(param.id);
+      this._firebase.getDataSessionCurrent(param.id);
     });
   }
 
   ngOnInit() {
-  }
-
-  // Recorrer la seccion del objeto session y agregar cada valor de Data en un array dataSession, para mostrar
-  // los datos en una tabla con un ngFor.
-  getData() {
-    Object.values(this.session.data).forEach( value => {
-      this.dataSession.push(value);
-    });
   }
 
 }
