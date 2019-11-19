9bd88c0aff6f841bbddcc0cd54aa249d9561cd26
sergioeabarcaf <sergioaf1991@gmail.com>
2019-11-19 14:36:00 -0300

    webModulo - creacion de home y graph, rutas y uso de bootstrap

--- webModulo/src/app/app-routing.module.ts
+++ webModulo/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GraphComponent } from './pages/graph/graph.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'graph/:id/:param', component: GraphComponent},
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
