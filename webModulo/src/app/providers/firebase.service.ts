b7af5533b46c0105e3849b84180e4dd780ea994b
sergioeabarcaf <sergioaf1991@gmail.com>
2019-11-21 02:08:07 -0300

    webModulo - uso de ultimos valores en Gauge

--- webModulo/src/app/providers/firebase.service.ts
+++ webModulo/src/app/providers/firebase.service.ts
@@ -7,11 +7,25 @@ import { Observable } from 'rxjs';
 })
 export class FirebaseService {
 
+  module = 'thermal';
+  show = true;
+  data: any;
+  lastData: any;
+
   constructor(public _firebase: AngularFireDatabase) { }
 
-  getInfoSessionsShort() {
-    this._firebase.list('info/short', ref => ref.orderByChild('startTimestamp')).valueChanges().subscribe( (data: any[]) => {
+  getActiveSession() {
+    this._firebase.object(`init/${this.module}`).valueChanges().subscribe( (data:any) => {
       console.log(data);
+      this.show = true;
+    });
+  }
+
+  getDataSessionActive(numberSession) {
+    this._firebase.list(`data/S-${numberSession}/${this.module}`).valueChanges().subscribe ( (data:any) => {
+      this.data = data.reverse();
+      this.lastData = this.data[0];
+      console.log(this.lastData);
     });
   }
 }
