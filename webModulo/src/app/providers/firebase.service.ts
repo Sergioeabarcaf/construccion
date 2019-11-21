625ba1499f90be9f51e98a3cd533a275eadf50aa
sergioeabarcaf <sergioaf1991@gmail.com>
2019-11-21 01:07:18 -0300

    webModulo - uso de firebase

--- webModulo/src/app/providers/firebase.service.ts
+++ webModulo/src/app/providers/firebase.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  constructor(public _firebase: AngularFireDatabase) { }
+
+  getInfoSessionsShort() {
+    this._firebase.list('info/short', ref => ref.orderByChild('startTimestamp')).valueChanges().subscribe( (data: any[]) => {
+      console.log(data);
+    });
+  }
 }
