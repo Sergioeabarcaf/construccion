8be76c838628e13aaf1f3554d20c69fec090d127
sergioeabarcaf <sergioaf1991@gmail.com>
2019-12-03 09:54:53 -0300

    webModulo - uso de getStatus desde app.component

--- webModulo/src/app/app.component.ts
+++ webModulo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../app/providers/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webModulo';
+
+  constructor( public _firebase:FirebaseService ) {
+    this._firebase.getStatus();
+  }
 }
--- webModulo/src/app/providers/firebase.service.ts
+++ webModulo/src/app/providers/firebase.service.ts
@@ -8,25 +8,63 @@ import { Observable } from 'rxjs';
 export class FirebaseService {
 
   module = 'thermal';
-  show = true;
-  numberSession:number = 89;
+  show = null;
+  sessionNumber:number = 89;
   data = [];
   lastData: any;
   infoSession = [];
+  responsable = 'Responsable';
+  material = 'Material';
+  status = "Estado"
 
   constructor(public _firebase: AngularFireDatabase) { }
+
+  getStatus() {
+    this._firebase.object(`system/status/${this.module}`).valueChanges().subscribe( (data:any) => {
+      console.log(data);
+      switch(data.value) {
+        case 0:
+          // this.show = false;
+          this.show = true;
+          break;
+        case 1:
+          this.status = 'Inicio Web';
+          this.show = true;
+          this.sessionNumber = data.sessionNumber;
+          break;
+        case 2:
+          this.status = 'Midiendo';
+          this.show = true;
+          this.sessionNumber = data.sessionNumber;
+          break;
+        case 3:
+          this.status = 'Fin Web';
+          this.show = true;
+          this.sessionNumber = data.sessionNumber;
+          break;
+        default:
+          this.show = true;
+          this.status = String(data.value);
+          break;
+      }
+    });
+  }
 
   getActiveSession() {
     this._firebase.object(`init/${this.module}`).valueChanges().subscribe( (data:any) => {
       // if ( data != null ) { 
       //   this.show = true;
       //   this.numberSession = data.sessionNumber;
+      //   this.material = data.material;
+      //   this.responsable = data.startResponsable;
       // } else {
       //   this.show = false;
       // }
       this.show = true;
     });
   }
+
+
 
   getDataSessionActive() {
     this._firebase.list(`data/S-${this.numberSession}/${this.module}`).valueChanges().subscribe ( (info:any[]) => {
